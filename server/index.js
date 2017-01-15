28125ed78d05fb6cc3fb222bad353e95e7dbbe37
Murali manohar <muralimano28@gmail.com>
2017-01-15 23:26:53 +0530

    Created a server which will consume a GET request with params and will send back data after fetching it from API_URL

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const Express = require('express');
+const Request = require('request');
+const BodyParser = require('body-parser');
+
+const API_URL = "http://yourmoneyisnowmymoney.com/api/zipcodes/?zipcode=";
+
+var App = Express();
+
+// Middlewares
+App.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+App.use(BodyParser.urlencoded({ extended: true }));
+
+App.get("/:zipcode", function(req, res) {
+	Request.get(API_URL + req.params.zipcode, function (error, response, body) {
+		res.send(body);
+	});
+});
+
+App.listen(3000, function(){
+	console.log("Listening at port 3000");
+});
